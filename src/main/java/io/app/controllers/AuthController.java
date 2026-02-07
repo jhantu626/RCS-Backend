e72b48ca1cf536c812beca442831f8a7489501b9
jhantu626 <jhantubala626@gmail.com>
2026-02-07 18:30:12 +0530

    bug resolved

--- src/main/java/io/app/controllers/AuthController.java
+++ src/main/java/io/app/controllers/AuthController.java
@@ -6,6 +6,7 @@ import io.app.dto.UserDto;
 import io.app.model.User;
 import io.app.services.impl.AuthServiceImpl;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,13 +21,15 @@ public class AuthController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
     public ApiResponse createUser(@RequestBody UserDto userDto){
         return service.createUser(userDto);
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public AuthResponse login(String username,String password){
+    public AuthResponse login(@RequestParam String username,
+                              @RequestParam String password){
         return service.login(username,password);
     }
 
