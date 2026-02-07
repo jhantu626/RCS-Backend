8f379fd3dc09e558a348e3a8cce3b3f353ed62bf
jhantu626 <jhantubala626@gmail.com>
2026-02-07 12:53:43 +0530

    added the bot functions

--- src/main/java/io/app/services/BotService.java
+++ src/main/java/io/app/services/BotService.java
@@ -2,7 +2,9 @@ package io.app.services;
 
 import io.app.dto.ApiResponse;
 import io.app.model.Bot;
+import org.springframework.data.domain.Page;
 
 public interface BotService {
-
+    public ApiResponse createBot(Bot bot);
+    public Page<Bot> getBots();
 }
