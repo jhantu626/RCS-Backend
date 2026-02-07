73db516af0f1288bf38ca5f57d3da1767fb11451
jhantu626 <jhantubala626@gmail.com>
2026-02-07 12:43:37 +0530

    first commit

---
+++ src/main/java/io/app/RcsApplication.java
@@ -0,0 +1,13 @@
+package io.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcsApplication.class, args);
+	}
+
+}
