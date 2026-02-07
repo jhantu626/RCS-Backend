d62a9b72d47485593ced4e6444ab0a9fdf322371
jhantu626 <jhantubala626@gmail.com>
2026-02-07 15:26:29 +0530

    added SecurityConfig

---
+++ src/main/java/io/app/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package io.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->
+                                auth.requestMatchers("/api/v1/bot/**")
+                                        .permitAll());
+        return http.build();
+    }
+
+}
