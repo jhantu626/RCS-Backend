2183c26d7c66a328b01ff5c00a36f9c1b6f0d1aa
jhantu626 <jhantubala626@gmail.com>
2026-02-07 16:30:51 +0530

    created user repository and one to get by username

--- src/main/java/io/app/repository/UserRepository.java
+++ src/main/java/io/app/repository/UserRepository.java
@@ -3,5 +3,8 @@ package io.app.repository;
 import io.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    public Optional<User> findUserByUserName(String username);
 }
