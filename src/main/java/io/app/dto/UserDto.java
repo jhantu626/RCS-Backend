10aa5b210dce120ed89a7b8a6c076aa1edd353f6
jhantu626 <jhantubala626@gmail.com>
2026-02-07 17:06:46 +0530

    created user dto

---
+++ src/main/java/io/app/dto/UserDto.java
@@ -0,0 +1,39 @@
+package io.app.dto;
+
+import io.app.model.BillingType;
+import io.app.model.Role;
+import io.app.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class UserDto {
+    private Long id;
+    private String userName;
+    private String password;
+    private Role role;
+    private BillingType billingType;
+    private boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    User mapToUser(){
+        return User.builder()
+                .id(this.id)
+                .userName(this.userName)
+                .password(this.password)
+                .role(this.role)
+                .billingType(this.billingType)
+                .isActive(this.isActive)
+                .updatedAt(this.updatedAt)
+                .createdAt(this.createdAt)
+                .build();
+    }
+}
